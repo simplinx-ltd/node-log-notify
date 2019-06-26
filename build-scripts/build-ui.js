61f27cd2787ad7d529c8d1d49d3eeed551df0724
Murat Dogan <doganmurat@gmail.com>
2019-06-26 15:34:22 +0300

    init ui

--- build-scripts/build-ui.js
+++ build-scripts/build-ui.js
@@ -5,7 +5,4 @@ console.log('>>       Building UI - START           <<');
 // Webpack
 execSync('cd src/ui; node_modules/.bin/ng build --prod', { stdio: 'inherit' });
 
-// Copy Required files/folders
-//execSync('(rm -r ./build/public/static; mkdir ./build/public/static; cp -r ./src/ui/modules/static/* ./build/public/static ; cp -r ./node_modules/bootstrap/dist ./build/public/static/bootstrap ; cp -r ./node_modules/amcharts3 ./build/public/static/)', { stdio: 'inherit' });
-
 console.log('>>       Building UI - END             <<');
--- build-scripts/build.js
+++ build-scripts/build.js
@@ -2,5 +2,5 @@ require('./clean.js');
 console.log('\n');
 require('./build-server.js');
 console.log('\n');
-// require('./build-ui.js');
-// console.log('\n');
+require('./build-ui.js');
+console.log('\n');
