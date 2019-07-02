e1f481596cc9047f1536102c43f4a1b003c497e1
Eren Esmer <erenesmerz@gmail.com>
2019-07-02 11:32:10 +0300

    Created Notification Service

--- src/ui/src/app/services/notification.service.ts
+++ src/ui/src/app/services/notification.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
+
+import { UserService } from './user.service';
 
 @Injectable({
 	providedIn: 'root'
 })
+
 export class NotificationService {
 
-	constructor() { }
+	constructor(private httpClient: HttpClient, private userService: UserService) { }
+
+	getNotificationAll() {
+		this.httpClient.get('/api/notification', {
+			headers: {
+				'x-access-token': this.userService.getToken()
+			}
+		})
+			.pipe(
+				map(res => res),
+				catchError(err => throwError(err))
+			);
+	}
+
+	getNotificationCount() {
+		this.httpClient.get('/api/notification/count', {
+			headers: {
+				'x-access-token': this.userService.getToken()
+			}
+		})
+			.pipe(
+				map(res => res),
+				catchError(err => throwError(err))
+			);
+	}
+
+	getNotificationById(id: number) {
+		this.httpClient.get('/api/notification/' + id, {
+			headers: {
+				'x-access-token': this.userService.getToken()
+			}
+		})
+			.pipe(
+				map(res => res),
+				catchError(err => throwError(err))
+			);
+	}
+
 }
