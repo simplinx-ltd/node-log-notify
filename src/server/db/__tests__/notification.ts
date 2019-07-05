0d6ab8987402ae49f2dfaee96ffc5015e974f677
Murat Dogan <doganmurat@gmail.com>
2019-07-05 17:05:21 +0300

    tests

--- src/server/db/__tests__/notification.ts
+++ src/server/db/__tests__/notification.ts
@@ -36,4 +36,87 @@ describe('Notification Model Operations', (): void => {
             });
         });
     });
+
+    describe('getNotifications', (): void => {
+        test('Calls findAll function', (done): void => {
+            mockedNotification.findAll.mockReturnValueOnce(BlueBird.resolve([]));
+            getNotifications({}).then((): void => {
+                expect(mockedNotification.findAll.mock.calls.length).toBe(1);
+                done();
+            });
+        });
+
+        test('Calls findAll with where & limit options', (done): void => {
+            let whereOptions = { id: 1 };
+            let limit = 10;
+            mockedNotification.findAll.mockReturnValueOnce(BlueBird.resolve([]));
+            getNotifications(whereOptions, limit).then((): void => {
+                expect(mockedNotification.findAll.mock.calls.length).toBe(1);
+                expect(mockedNotification.findAll.mock.calls[0][0].where).toEqual(whereOptions);
+                expect(mockedNotification.findAll.mock.calls[0][0].limit).toEqual(limit);
+                done();
+            });
+        });
+
+        test('Returns result', (done): void => {
+            let result = new Notification();
+            result.processName = 'ProcessName';
+            mockedNotification.findAll.mockReturnValueOnce(BlueBird.resolve([result]));
+            getNotifications({}).then((res): void => {
+                expect(mockedNotification.findAll.mock.calls.length).toBe(1);
+                expect(Array.isArray(res)).toBeTruthy();
+                expect(res.length).toBe(1);
+                expect(res[0]).toEqual(result);
+                done();
+            });
+        });
+
+        test('Rejects on error', (done): void => {
+            mockedNotification.findAll.mockReturnValueOnce(BlueBird.reject('Reject Error'));
+            getNotifications({}).catch((e): void => {
+                expect(mockedNotification.findAll.mock.calls.length).toBe(1);
+                expect(e).toBe('Reject Error');
+                done();
+            });
+        });
+    });
+
+    describe('getNotificationsCount', (): void => {
+        test('Calls count function', (done): void => {
+            mockedNotification.count.mockReturnValueOnce(BlueBird.resolve(1));
+            getNotificationsCount({}).then((): void => {
+                expect(mockedNotification.count.mock.calls.length).toBe(1);
+                done();
+            });
+        });
+
+        test('Calls count with where options', (done): void => {
+            let whereOptions = { id: 1 };
+            mockedNotification.count.mockReturnValueOnce(BlueBird.resolve(10));
+            getNotificationsCount(whereOptions).then((): void => {
+                expect(mockedNotification.count.mock.calls.length).toBe(1);
+                expect(mockedNotification.count.mock.calls[0][0].where).toEqual(whereOptions);
+                done();
+            });
+        });
+
+        test('Returns result', (done): void => {
+            let count = 125;
+            mockedNotification.count.mockReturnValueOnce(BlueBird.resolve(count));
+            getNotificationsCount({}).then((res): void => {
+                expect(mockedNotification.count.mock.calls.length).toBe(1);
+                expect(res).toEqual(count);
+                done();
+            });
+        });
+
+        test('Rejects on error', (done): void => {
+            mockedNotification.count.mockReturnValueOnce(BlueBird.reject('Reject Error'));
+            getNotificationsCount({}).catch((e): void => {
+                expect(mockedNotification.count.mock.calls.length).toBe(1);
+                expect(e).toBe('Reject Error');
+                done();
+            });
+        });
+    });
 });
