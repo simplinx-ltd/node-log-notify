17b52ea553598a7ec52ff9295379735be7e0a59e
Eren Esmer <erenesmerz@gmail.com>
2019-07-16 15:27:44 +0300

    Created Mock Middleware for API Tests

--- src/server/api/__tests__/resource-cpu.ts
+++ src/server/api/__tests__/resource-cpu.ts
@@ -4,24 +4,31 @@ import { Request, Response, NextFunction } from 'express';
 import * as express from 'express';
 import * as request from 'supertest';
 import api from '../resource-cpu';
+import { authMiddleware } from '../auth';
 
 jest.mock('sx-sequelize-api');
+jest.mock('../auth');
 
 const mockedSxApi = mocked(sxApi, false);
 const mockedModelApi = mocked(mockedSxApi.ModelRestApi, false);
+const mockedAuth = mocked(authMiddleware, false);
 
 mockedModelApi.mockImplementation(
     (): sxApi.ModelRestApi => {
         function getAll(): (req: Request, res: Response, next: NextFunction) => void {
-            return (req: Request, res: Response, next: NextFunction) => {
+            return (req: Request, res: Response): Response => {
                 return res.json();
             };
         }
         function count(): (req: Request, res: Response, next: NextFunction) => void {
-            return () => {};
+            return (req: Request, res: Response): Response => {
+                return res.json();
+            };
         }
         function getById(): (req: Request, res: Response, next: NextFunction) => void {
-            return () => {};
+            return (req: Request, res: Response): Response => {
+                return res.json();
+            };
         }
         let mock = {
             getAll,
@@ -33,6 +40,12 @@ mockedModelApi.mockImplementation(
     },
 );
 
+mockedAuth.mockImplementation((): ((req: Request, res: Response, next: NextFunction) => void) => {
+    return (req: Request, res: Response, next: NextFunction): void => {
+        return next();
+    };
+});
+
 let app = express();
 app.use(api(null));
 
@@ -40,6 +53,24 @@ describe('ResourceCPU Endpoints', (): void => {
     test('Endpoint / OK', (done): void => {
         request(app)
             .get('/')
+            .then((response): void => {
+                expect(response.status).toBe(200);
+                done();
+            });
+    });
+
+    test('Endpoint /count OK', (done): void => {
+        request(app)
+            .get('/count')
+            .then((response): void => {
+                expect(response.status).toBe(200);
+                done();
+            });
+    });
+
+    test('Endpoint /:id OK', (done): void => {
+        request(app)
+            .get('/1')
             .then((response): void => {
                 expect(response.status).toBe(200);
                 done();
